4e17df3f1c37b0d975b4062249666fbd1392f90a
Max Woghiren <max.woghiren@gmail.com>
2025-06-01 16:03:48 -0400

    Remove checks.

--- settings/prod.py
+++ settings/prod.py
@@ -5,7 +5,3 @@ from settings.base import SITE_PASSWORD
 DEBUG = False
 
 ALLOWED_HOSTS = ["https://iph2-puzzlord-34c662c2c86a.herokuapp.com"]
-
-# security checks
-assert SECRET_KEY != "FIXME_SECRET_KEY_GOES_HERE"
-assert SITE_PASSWORD != "FIXME_PASSWORD_GOES_HERE"
